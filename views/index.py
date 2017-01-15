f5991377bf51a64e69dcb0392419de5af986327d
betalun <betalun@qq.com>
2017-01-15 09:10:18 +0800

    add

--- views/index.py
+++ views/index.py
@@ -1,9 +1,6 @@
-from . import app
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/restpy/v0.1/tasker',methods=['GET'])
 def taskList():
-    pass
-
-
-# if __name__ =='__main__':
-#     app.run(8888);
+    return 'hello'
